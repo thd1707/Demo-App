1ef0489537fef257ba2bd7d3f1ed2e7eb3de1757
Hong Duc <tranhongduchkn@gmail.com>
2024-06-23 12:04:51 +0700

    update

--- Đèn xi nhan 2/ViewController.swift
+++ Đèn xi nhan 2/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblStatus: UILabel!
+    
+    @IBOutlet weak var btnSignalLight: UIButton!
+    
+    @IBAction func sliderChangeValue(_ sender: Any) {
+    }
+    
+    
+    var isFlashing = false
+    var flashTimer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lblStatus.text = "Xi nhan đã tắt"
+        lblStatus.textColor = .red
+        btnSignalLight.setTitle("Bật xi nhan", for: .normal)
+        
+      
     }
-
-
+    
+    @IBAction func btnFlicker(_ sender: UIButton) {
+        isFlashing.toggle()
+        if isFlashing {
+            startFlashing()
+            btnSignalLight.setTitle("Tắt xi nhan", for: .normal)
+            lblStatus.text = "Xi nhan đang bật"
+            lblStatus.textColor = .green
+        } else {
+            stopFlashing()
+            btnSignalLight.setTitle("Bật xi nhan", for: .normal)
+            lblStatus.text = "Xi nhan đã tắt"
+            lblStatus.textColor = .red
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        if isFlashing {
+            stopFlashing()
+            startFlashing()
+        }
+    }
+    
+    func startFlashing() {
+//        let interval = TimeInterval(sliderChangeValue.value)
+//        flashTimer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { [weak self] _ in
+//            guard let self = self else { return }
+//            self.view.backgroundColor = (self.view.backgroundColor == .white) ? .black : .white
+//        }
+    }
+    
+    func stopFlashing() {
+        flashTimer?.invalidate()
+        flashTimer = nil
+        view.backgroundColor = .white
+    }
 }
-
